a08834de1791af2eec34c1d99a7cd7e9f3fac29e
popov <popov@adn.agency>
2020-09-10 18:06:33 +0700

    add sle ep in start file

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
-composer install 
+#!/usr/bin/env bash
+composer install
 cp .env.example .env
 php artisan key:generate
 docker-compose up -d
+sleep 5
 docker-compose exec app php artisan migrate
 docker-compose exec app php artisan passport:install
